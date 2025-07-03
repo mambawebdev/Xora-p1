bc6ce331570b3a844394de43eb32b412e701956b
Tony L <155401134+mambawebdev@users.noreply.github.com>
2025-07-03 18:14:20 -0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from './sections/Header'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import Pricing from './sections/Pricing'
+import Faq from './sections/Faq'
+import Testimonials from './sections/Testimonials'
+
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+    </main>
+  )
+}
+
+export default App
+
